5c67a5b08a35a56e0cec87ed1371d72ea78f2d19
bigcloud <bigcloud@bigcloudmac.local>
2026-01-20 11:34:10 +0900

    feat: 당첨 번호 입력 기능 구현

--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,6 @@
 import { LottoStore } from "./model/LottoStroe.js";
 import { inputErrorHandler } from "./utils/errorHandler.js";
-import { validateUnitAmount } from "./utils/validator.js";
+import { validateLottoNumberLength, validateLottoNumberRange, validateUnitAmount } from "./utils/validator.js";
 import { InputView, OutputView } from "./view.js";
 
 export default async function controller() {
@@ -14,10 +14,13 @@ export default async function controller() {
 
     const lottoCount = amount / 500;
     const issuedLottos = lottoStore.generateLotto(lottoCount);
+    console.log(issuedLottos);
     OutputView.printPurchasedLottos(issuedLottos);
 
-    // const winningLotto = await inputErrorHandler(async() => {
-    //     const inputWinningLotto = await InputView.askWinningLotto();
-    //     return inputWinningLotto;
-    // }, OutputView)
+    const winningLotto = await inputErrorHandler(async() => {
+        const inputWinningLotto = await InputView.askWinningLotto();
+        validateLottoNumberRange(inputWinningLotto);
+        validateLottoNumberLength(inputWinningLotto);
+        return inputWinningLotto;
+    }, OutputView);
 }
--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,7 +1,20 @@
 export function validateUnitAmount(amount) {
     const isUnitAmount500 = (amount % 500 === 0) && (amount > 0);
-    if(!isUnitAmount500){
+    if (!isUnitAmount500) {
         throw new Error('로또는 500원 단위로 구입할 수 있습니다.');
     }
 }
 
+export function validateLottoNumberRange(lotto) {
+    const isLottoNumberRange = lotto.every((number) => number >= 0 && number <= 30);
+    if (!isLottoNumberRange) {
+        throw new Error('로또 번호는 1부터 30 사이의 숫자여야 합니다.');
+    }
+}
+
+export function validateLottoNumberLength(lotto) {
+    const isLottoNumberLength = (lotto.length == 5);
+    if(!isLottoNumberLength) {
+        throw new Error('로또 번호는 5개를 입력해 주셔야 합니다.')
+    }
+}
